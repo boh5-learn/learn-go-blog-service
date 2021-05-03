55b4db4fab29a376d32a05449d1e7e9499e34bd6
dilless <bohuang1994@gmail.com>
2021-05-03 16:24:40 +0800

    2.3 公共组件

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module blog-service
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.7.1
+require (
+	github.com/gin-gonic/gin v1.7.1
+	github.com/spf13/viper v1.7.1
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gorm.io/driver/mysql v1.0.6
+	gorm.io/gorm v1.21.9
+)
--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,16 @@
 package model
 
+import (
+	"blog-service/global"
+	"blog-service/pkg/setting"
+	"errors"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreatedBy  string `json:"created_by"`
@@ -8,4 +19,37 @@ type Model struct {
 	ModifiedOn uint32 `json:"modified_on"`
 	DeletedOn  uint32 `json:"deleted_on"`
 	IsDel      uint8  `json:"is_del"`
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local",
+		databaseSetting.Username,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime)
+
+	var db *gorm.DB
+	var err error
+	switch databaseSetting.DBType {
+	case "mysql":
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	default:
+		err = errors.New(fmt.Sprintf("不识别的连接类型: %s", databaseSetting.DBType))
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	if global.ServerSetting.RunMode == "debug" {
+		db.Logger.LogMode(logger.Info)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	sqlDB.SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	sqlDB.SetMaxOpenConns(databaseSetting.MaxOpenConns)
+	return db, nil
 }
